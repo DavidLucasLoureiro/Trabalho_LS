ec0615210fe3d49a70c22d7a98ad5e335188027b
DavidLucasLoureiro <davidlucasloureiro@gmail.com>
2024-05-05 15:44:24 +0100

    bootstrap installation and something

--- ls/src/App.js
+++ ls/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import GameComponent from "./game-component/game-component.js";
 function App() {
   return (
     <div>
-        <h1>Hello World!</h1>
+        <GameComponent />
     </div>
   );
 }
---
+++ ls/src/game-component/game-component.js
@@ -0,0 +1,7 @@
+import "./game-component.css";
+
+function GameComponent(){
+    return(<h1>Hello World!</h1>);
+}
+
+export default GameComponent;
