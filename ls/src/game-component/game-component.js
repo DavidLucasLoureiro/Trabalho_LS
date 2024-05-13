fd5693d8d2d9068d208556612fc7d9c6ca5035d3
David Loureiro <davidlucasloureiro@gmail.com>
2024-05-13 18:57:19 +0100

    teste

--- ls/src/game-component/game-component.js
+++ ls/src/game-component/game-component.js
@@ -1,7 +1,7 @@
 import "./game-component.css";
 
 function GameComponent(){
-    return(<h1><b>Hello World</b> and something I guess bsb</h1>);
+    return(<h1><b>Hello World</b> and something I guess vyugikhvghfcdryu</h1>);
 }
 
 export default GameComponent;
