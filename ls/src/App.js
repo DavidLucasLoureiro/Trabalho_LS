c509ffaf95ff6971e67f28122c36e3e9ff0d4693
Rodrigo Cabacos <rodrigo.cabacos@gmail.com>
2024-05-18 19:24:22 +0100

    amen

--- ls/src/App.js
+++ ls/src/App.js
@@ -1,11 +1,12 @@
 import './assets/styles/App.css';
-import { Timer, Board, Cell, Modal, Header, Footer } from "./components";
-
+import Timer from "./components/timer/timer.component";
+import Board from "./components/board/board.component";
+import Cell from "./components/cell/cell.component";
+import Modal from "./components/game-over-modal/game-over-modal.component";
 
 function App() {
   return (
     <div id="container">
-      <Header />
       <main>
         <Timer
           
@@ -15,7 +16,6 @@ function App() {
         />
 
       </main>
-      <Footer />
     </div>
   );
 }
