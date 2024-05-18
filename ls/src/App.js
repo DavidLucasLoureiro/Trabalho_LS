90501803a37d9f93979aecd4d6e8d6b8bcf38c7f
David Loureiro <davidlucasloureiro@gmail.com>
2024-05-18 17:47:28 +0100

    .

--- ls/src/App.js
+++ ls/src/App.js
@@ -1,4 +1,3 @@
-import ControlPanel from "./components/control-panel/control-panel.component";
 import GamePanel from "./components/game-panel/game-panel.component";
 import './assets/styles/App.css';
 
--- ls/src/components/control-panel/control-panel.component.jsx
+++
@@ -1,8 +0,0 @@
-import "./control-panel.css";
-import React from "react";
-// modal no video
-export default function ControlPanel(){
-    return(<div CssClass="control">
-        
-    </div>);
-}
