8a838accd07c7cbd02cb3ca8b6354f66fe405f9f
Rodrigo Cabacos <rodrigo.cabacos@gmail.com>
2024-05-18 20:40:42 +0100

    dasdsad

--- ls/src/App.js
+++ ls/src/App.js
@@ -3,8 +3,11 @@ import Timer from "./components/timer/timer.component";
 import Board from "./components/board/board.component";
 import Cell from "./components/cell/cell.component";
 import Modal from "./components/game-over-modal/game-over-modal.component";
+import { useState } from 'react'
 
 function App() {
+  const [gameStarted, setGameStarted] = useState(false);
+
   return (
     <div id="container">
       <main>
--- ls/src/components/board/board.component.jsx
+++ ls/src/components/board/board.component.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { useState, ueef } from "react";
 
 export default function Board() {
+    let board = [];
+    const rows = 9;
+    const columns = 9;
+
+
+    for(let i = 0; i < rows; i++){
+        board.push([]);x
+        for (let j = 0; j < columns; j++) { 
+            board[i].push({
+               x: j,
+               y: i,
+               bombs: 0,
+               isOpen: false,
+               hasMine: false,
+               hasFlag: false,
+            });
+        }    
+    }
+    
   return (
     <footer>
       <p>© D E I S @ I S EC</p>
