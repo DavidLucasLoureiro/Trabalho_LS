3bcc2115bb709030575ec8e41e18b517239aff39
Rodrigo Cabacos <rodrigo.cabacos@gmail.com>
2024-05-18 19:02:58 +0100

    daws

--- ls/src/components/board/board.component.jsx
+++ ls/src/components/board/board.component.jsx
@@ -1,4 +1,11 @@
 import React from "react";
 
-function Board() {}
-export default Board;
+
+
+export default function Board() {
+  return (
+    <footer>
+      <p>© D E I S @ I S EC</p>
+    </footer>
+  );
+}
