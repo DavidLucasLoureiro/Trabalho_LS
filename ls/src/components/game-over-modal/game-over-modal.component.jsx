6a67f889b2a6e4201030d329561366e80b6a7d2f
David Loureiro <davidlucasloureiro@gmail.com>
2024-06-02 23:28:42 +0100

    game over modal (continuação)

--- ls/src/components/game-over-modal/game-over-modal.component.jsx
+++ ls/src/components/game-over-modal/game-over-modal.component.jsx
@@ -8,6 +8,10 @@ const GameOverModal = ({ game, resetGame, result }) => {
         <div className="game-over-modal">
             <div className="game-over-content">
                 <h1>{result ? "You Win!" : "You Lose!"}</h1>
+                {
+                    result ? (<img src="/img/win.jpg" alt=""/>):(<img src="/img/sad.gif" alt=""/>)
+                }
+                <br />
                 <button onClick={resetGame}>Restart</button>
             </div>
         </div>
