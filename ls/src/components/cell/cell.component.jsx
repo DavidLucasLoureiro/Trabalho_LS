c8003cbd2c2959fd660b676e792a56e10c341006
Pedro Águas <pedroaguas18@gmail.com>
2024-06-02 18:22:51 +0100

    cell-mudar2

--- ls/src/components/cell/cell.component.jsx
+++ ls/src/components/cell/cell.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './cell.css';
 
-const Cell = ({ x, y, isOpen, hasMine, hasFlag, bombs, onClick, onContextMenu, isGameOver }) => {
+const Cell = ({ x, y, isOpen, hasMine, hasFlag, bombs, onClick, onContextMenu }) => {
     let cellClass = 'cell';
     if (isOpen) cellClass += ' open';
 
@@ -11,11 +11,7 @@ const Cell = ({ x, y, isOpen, hasMine, hasFlag, bombs, onClick, onContextMenu, i
                 <img src="/img/bomb.png" alt="mine" className="mine-image" />
             ) : (
                 hasFlag ? (
-                    isGameOver && hasMine ? (
-                        <img src="/img/bomb-correct.png" alt="correct flag" className="flag-image" />
-                    ) : (
-                        <img src="/img/flag.png" alt="flag" className="flag-image" />
-                    )
+                    <img src="/img/flag.png" alt="flag" className="flag-image" />
                 ) : (
                     isOpen && bombs > 0 ? bombs : ''
                 )
